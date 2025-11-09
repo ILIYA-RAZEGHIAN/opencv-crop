659050a1650c15059d5b79c5eceb185c2d24f6f2
ILIYA-RAZEGHIAN <iliarazeghianja@gmail.com>
2025-11-09 11:05:56 +0330

    Update ss.py

--- ss.py
+++ ss.py
@@ -1,6 +1,6 @@
 import cv2
 import matplotlib.pyplot as plt
-image=cv2.imread('c:/users/iliya/downloads/1.jpg')
+image=cv2.imread('enter you photo file location') #'c:/users/iliya/downloads/1.jpg'
 plt.imshow(image[:,:,::-1])
 plt.show()
 sheight,eheight,swidth,ewidth=map(int,input('enter').split())
@@ -8,3 +8,4 @@ cropped=image[sheight:eheight,swidth:ewidth,0:3]
 cv2.imshow('cropped',cropped)
 cv2.waitKey(0)
 cv2.imwrite('khar.jpg',cropped)
+
